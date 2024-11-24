b36d3bfcb711dbba89f1c9904042e8d3065a8d08
luiscclavijo <luismclavijo22@gmail.com>
2024-11-24 13:41:30 -0500

    Update erp.py

--- erp.py
+++ erp.py
@@ -21,8 +21,10 @@ st.write("Bienvenido al sistema ERP para la gestión de clientes, inventarios, f
 st.sidebar.title("SOMOS TENTACIÓN")
 
 # Variables de autenticación
-USER = "luiscclavijo" "marlyycalderon"
-PASSWORD = "2200Luis" "2200Marly"
+USER = "luiscclavijo" 
+USER = "marlyycalderon"
+PASSWORD = "2200Luis" 
+PASSWORD = "2200Marly"
 
 # Inicialización de variables globales
 if "auth" not in st.session_state:
